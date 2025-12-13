c2b2c6e837e7ab31750849aa4eed0b60c937395d
Jas0n <136178185+Jas0nOW@users.noreply.github.com>
2025-12-13 11:56:33 +0100

    Staff Implementiert

--- Includes/Repository/StaffExceptionsRepository.php
+++ Includes/Repository/StaffExceptionsRepository.php
@@ -2,14 +2,48 @@
 
 class StaffExceptionsRepository {
     public function get_range($user_id, $from, $to) {
-        // Implementation to get exceptions for the user in the given range
+        global $wpdb;
+        $table = $wpdb->prefix . 'lazy_staff_exceptions';
+        $user_id = (int) $user_id;
+
+        $results = $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT id, user_id, date, is_off_day, start_time, end_time, note FROM $table WHERE user_id = %d AND date BETWEEN %s AND %s ORDER BY date ASC",
+                $user_id, $from, $to
+            ),
+            ARRAY_A
+        );
+
+        return $results ?: [];
     }
 
     public function create($user_id, $date, $is_off_day, $start_time, $end_time, $note) {
-        // Implementation to create a new exception
+        global $wpdb;
+        $table = $wpdb->prefix . 'lazy_staff_exceptions';
+        $user_id = (int) $user_id;
+        $is_off_day = !empty($is_off_day) ? 1 : 0;
+
+        $wpdb->insert(
+            $table,
+            [
+                'user_id' => $user_id,
+                'date' => $date,
+                'is_off_day' => $is_off_day,
+                'start_time' => $start_time,
+                'end_time' => $end_time,
+                'note' => $note
+            ],
+            ['%d','%s','%d','%s','%s','%s']
+        );
+
+        return $wpdb->insert_id ?: false;
     }
 
     public function delete($id) {
-        // Implementation to delete an exception
+        global $wpdb;
+        $table = $wpdb->prefix . 'lazy_staff_exceptions';
+        $id = (int) $id;
+
+        return (bool) $wpdb->delete($table, ['id' => $id], ['%d']);
     }
 }
